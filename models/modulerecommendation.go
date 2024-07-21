d86e6c568ebd571c4ce0c1caeeeedbcdc735ed65
Ivan Ramirez Comas <70542717+IvanR-C@users.noreply.github.com>
2024-07-21 00:10:33 -0400

    updated models last time

---
+++ models/modulerecommendation.go
@@ -0,0 +1,29 @@
+package models
+
+// ModuleRecommendationResponse represents the response object structure
+type ModuleRecommendationResponse struct {
+	Descendant DescendantRecommendation `json:"descendant"`
+	Weapon     WeaponRecommendation     `json:"weapon"`
+}
+
+// DescendantRecommendation represents descendant recommendation information
+type DescendantRecommendation struct {
+	DescendantID    string             `json:"descendant_id"`
+	Recommendations []DescendantModule `json:"recommendation"`
+}
+
+// DescendantModule represents module recommendation information for a descendant
+type DescendantModule struct {
+	ModuleID string `json:"module_id"`
+}
+
+// WeaponRecommendation represents weapon recommendation information
+type WeaponRecommendation struct {
+	WeaponID        string         `json:"weapon_id"`
+	Recommendations []WeaponModule `json:"recommendation"`
+}
+
+// WeaponModule represents module recommendation information for a weapon
+type WeaponModule struct {
+	ModuleID string `json:"module_id"`
+}
