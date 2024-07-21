7b9bcaf549c4cec4e1eaa3e8bef69eaa4c6bff9a
guisaez <guillesaez15@icloud.com>
2024-07-21 13:44:46 -0400

    Starting metadata service fucntions

---
+++ language_code.go
@@ -0,0 +1,17 @@
+package tfd
+
+type LanguageCode string
+
+var LanguageCodes struct {
+	Korean            LanguageCode
+	English           LanguageCode
+	Danish            LanguageCode
+	French            LanguageCode
+	Japanese          LanguageCode
+	ChineseSimplified LanguageCode
+	Italian           LanguageCode
+	Polish            LanguageCode
+	Portuguese        LanguageCode
+	Russian           LanguageCode
+	Spanish           LanguageCode
+}
